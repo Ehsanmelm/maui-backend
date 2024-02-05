b97b196786e81d202d0c9feb4ec1b6cacee1a15e
Ehsan <ehsanmelm82@gmail.com>
2024-02-06 02:42:40 +0330

    feat : create event models

---
+++ event/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from core.models import user
+# Create your models here.
+
+
+class EventModel(models.Model):
+    title = models.CharField(max_length = 255)
+    description = models.TextField()
+    event_maker = models.ForeignKey(user , on_delete = models.CASCADE)
+
+class EventUserModel(models.Model):
+    event_picker = models.ForeignKey(user , on_delete=models.CASCADE , default = None)
+    event = models.ManyToManyField(EventModel)
