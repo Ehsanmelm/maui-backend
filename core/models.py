f4529172d826e8a5b5c301f0e9701a801c673982
Ehsan <ehsanmelm82@gmail.com>
2024-02-06 02:41:23 +0330

    feat : create user model

---
+++ core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class user(models.Model):
+    email = models.EmailField( default =None)
+    name = models.CharField(max_length =255)
+    password = models.CharField(max_length = 255  , default= None)
+    age = models.CharField(max_length =255 , null = True , default = None)
+    phone = models.CharField(max_length =255 , null =True , default = None)
