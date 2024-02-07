0c1ac4e40f7fa60fa75a1cff0964010239ecbb36
Ehsan <ehsanmelm82@gmail.com>
2024-02-07 13:13:56 +0330

    feat : check for decreasign capacity for event after select that

--- event/serializers.py
+++ event/serializers.py
@@ -27,9 +27,23 @@ class EventUserSerializer(serializers.ModelSerializer):
         event_picker = UserModel.objects.get(id=self.context['id'])
         events_data = validated_data.pop('event', [])  # Remove 'event' from validated_data
 
-        event_user = EventUserModel.objects.create(event_picker=event_picker, **validated_data)
+        event_user , is_created = EventUserModel.objects.get_or_create(event_picker=event_picker, **validated_data)
 
         for event in events_data:
-            event_user.event.add(event)  # Add each event to the many-to-many relationship
 
+            selected_event = EventModel.objects.get(id= event.id)
+
+            if selected_event in event_user.event.all():
+                raise serializers.ValidationError("this event is in you selected event")
+            
+            else:
+                if selected_event.capacity > 0:
+
+                    selected_event.capacity -= 1
+                    selected_event.save()
+                    event_user.event.add(event)  # Add each event to the many-to-many relationship
+
+                else:
+                    raise serializers.ValidationError('event is full')
+            
         return event_user
